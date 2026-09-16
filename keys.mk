dad9f68d75992ec4ca08a700fc9e43e635637889
dreamsolister26 <oumaekumiko123@gmail.com>
2026-09-16 21:54:06 +0700

    keys: adapt to EvolutionX
    Signed-off-by: dreamsolister26 <oumaekumiko123@gmail.com>

--- keys.mk
+++ keys.mk
@@ -137,6 +137,6 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     WebAppService:com.android.webapp.certificate.override \
     WifiDialog:com.android.wifi.dialog.certificate.override
 
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
-PRODUCT_EXTRA_RECOVERY_KEYS += vendor/lineage-priv/keys/signed
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/evolution-priv/keys/releasekey
+PRODUCT_EXTRA_RECOVERY_KEYS += vendor/evolution-priv/keys/signed
 PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := $(dir $(PRODUCT_DEFAULT_DEV_CERTIFICATE))
